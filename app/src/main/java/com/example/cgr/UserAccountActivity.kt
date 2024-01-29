2208b25e0ac16b85da42eab531907b39bc571268
RichardKoudou <richard.koudou@epsi.fr>
2024-01-29 16:59:03 +0100

    creation de la branche UserAccount ainsi que l'ajout du BaseActivity

---
+++ app/src/main/java/com/example/cgr/UserAccountActivity.kt
@@ -0,0 +1,43 @@
+package com.example.cgr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+//import kotlinx.android.synthetic.main.activity_user_account.*
+
+class UserAccountActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_account)
+
+        showBack()
+        setHeaderTitle(getString(R.string.txtUserAccount))
+
+        val editTextTextLastName=findViewById<EditText>(R.id.editTextTextLastName)
+        val editTextTextName=findViewById<EditText>(R.id.editTextTextName)
+        val buttonUpdate=findViewById<Button>(R.id.buttonUpdate)
+        val editTextTextEmail=findViewById<EditText>(R.id.editTextTextEmail)
+        val editTextTextAddress=findViewById<EditText>(R.id.editTextTextAddress)
+        val editTextTextZipCode=findViewById<EditText>(R.id.editTextTextZipCode)
+        val editTextTextCity=findViewById<EditText>(R.id.editTextTextCity)
+        buttonUpdate.setOnClickListener(View.OnClickListener {
+            Toast.makeText(application,editTextTextAddress.text.toString()+"/"
+                    +editTextTextEmail.text.toString()+"/"
+                    +editTextTextName.text.toString()+"/"
+                    +editTextTextLastName.text.toString()+"/"
+                    +editTextTextZipCode.text.toString()+"/"
+                    +editTextTextCity.text.toString()+"/",
+                Toast.LENGTH_LONG).show()
+        })
+
+
+
+
+    }
+}
