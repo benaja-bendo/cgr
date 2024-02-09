d09a41c4dfb557a7b888328380abc53e7eaae5b0
benji_mac <benaja.bendo02@gmail.com>
2024-02-09 17:41:56 +0100

    ✨ wip qr code

--- app/src/main/java/com/example/cgr/MainActivity.kt
+++ app/src/main/java/com/example/cgr/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.cgr
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultLancher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val qrCode = data?.getStringExtra(ScanQRActivity.QR_CODE_KEY)
+                val intent = Intent(this, FormManuelActivity::class.java)
+                intent.putExtra("QRCode", qrCode)
+                startActivity(intent)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,13 +28,19 @@ class MainActivity : AppCompatActivity() {
         val formManuelButton = findViewById<Button>(R.id.fillFormButton)
 
         scanQRButton.setOnClickListener {
-            val intent = Intent(this, ScanQRActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this, ScanQRActivity::class.java)
+//            startActivity(intent)
+            startScanQRActivity()
         }
         formManuelButton.setOnClickListener {
             val intent = Intent(this, FormManuelActivity::class.java)
             startActivity(intent)
         }
 
+    }
+
+    private fun startScanQRActivity() {
+        val intent = Intent(this, ScanQRActivity::class.java)
+        resultLancher.launch(intent)
     }
 }
